1e1c5bbb8a973dca456062641389497351b4e760
yuzi83 <huzhenshi7@gmail.com>
2026-02-20 15:27:35 +0800

    Update beautifier.js

--- modules/beautifier.js
+++ modules/beautifier.js
@@ -1,7 +1,7 @@
 // modules/beautifier.js
 /**
  * 玉子市场 - 美化器系统
- * @version 2.6.0
+ * @version 2.8.4
  */
 
 import { 
@@ -365,3 +365,4 @@ export function renderWithBeautifier($container, rawMessage, templateData) {
         return false;
     }
 }
+
